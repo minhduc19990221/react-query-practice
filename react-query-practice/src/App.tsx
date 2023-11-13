33e0ad425f8411acf37df564e93aebc1b8190871
[Minh Duc Nguyen Cao] <[duc-cao@cybozu.vn]>
2023-11-13 10:06:35 +0700

    init project

---
+++ react-query-practice/src/App.tsx
@@ -0,0 +1,36 @@
+import { useQuery } from "@tanstack/react-query";
+import axios from "axios";
+
+interface Todo {
+  id: number;
+  title: string;
+  userId: number;
+  completed: boolean;
+}
+
+const TodoList = () => {
+  const fetchTodos = () =>
+    axios
+      .get<Todo[]>("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.data);
+
+  const { data, error, isLoading } = useQuery<Todo[], Error>({
+    queryKey: ["todos"],
+    queryFn: fetchTodos,
+  });
+
+  if (error) return <p>{error.message}</p>;
+  if (isLoading) return <p>Loading...</p>;
+
+  return (
+    <ul className="list-group">
+      {data?.map((todo) => (
+        <li key={todo.id} className="list-group-item">
+          {todo.title}
+        </li>
+      ))}
+    </ul>
+  );
+};
+
+export default TodoList;
